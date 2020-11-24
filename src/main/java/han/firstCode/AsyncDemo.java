9a2f821c2902d929525b85d34cfe7f14b36162ee
zhaohanyang <zhaohanyang@atwasoft.net>
2020-11-24 14:36:00 +0800

    first

---
+++ src/main/java/han/firstCode/AsyncDemo.java
@@ -0,0 +1,30 @@
+/**
+ * @FileName: AsyncDemo.java
+ * @creator zhaohanyang
+ * @date Nov 24, 2020
+ * @editor
+ * @Description: 
+ * @version V1.0
+ */
+package han.firstCode;
+
+/**
+ * @ClassName: AsyncDemo
+ * @Description: 
+ * @author zhaohanyang
+ * @date Nov 24, 2020
+ * @version V1.0
+ */
+public class AsyncDemo {
+
+	private static String outString = "asyncBegin";
+	/**
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(outString);
+	}
+
+}
